6253c3ae73ec05d4ce4671ff8a100036854bbe1a
ALGIRDAS LAUKAITIS <algirdas.laukaitis@vgtu.lt>
2021-09-12 17:10:59 +0300

    Add files via upload

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,21 @@
+﻿// ClickToMove.cs
+using UnityEngine;
+
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+	RaycastHit hitInfo = new RaycastHit();
+	UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Vector3 mousePos = Input.mousePosition;
+			Debug.Log("mousePos : " + mousePos);
+			Ray ray = Camera.main.ScreenPointToRay(mousePos);
+			//if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+				//agent.destination = hitInfo.point;
+		}
+	}
+}
